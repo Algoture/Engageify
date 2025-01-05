7db02531c9022b46dfaa12b9aadb19d503eeac7d
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-05 22:45:39 +0530

    remove

--- constants.js
+++ constants.js
@@ -2,5 +2,3 @@ import { config as configDotenv } from "dotenv";
 configDotenv();
 export const token = process.env.TOKEN;
 export const dbapiendpoint = process.env.DB_API_ENDPOINT;
-export const clientId = `${process.env.CLIENT_ID}`;
-export const clientSecret = `${process.env.CLIENT_SECRET}`;
